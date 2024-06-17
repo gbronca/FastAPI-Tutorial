ec9d4fca9e91f5719ce8d63addf681a26716339b
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-06-17 20:20:07 +0100

    Query parameters and string validations

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
+
+from typing import Annotated
 
 
 class Item(BaseModel):
@@ -39,3 +41,89 @@ async def update_item(item_id: int, item: Item, q: str | None = None):
     if q:
         result |= {"q": q}
     return result
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# When you define a query parameter explicitly with Query you can also declare
+# it to receive a list of values, or said in other way, to receive multiple values.
+@app.get("/items/")
+async def read_items_list(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items
+
+
+# Then, with a URL like: http://localhost:8000/items/?q=foo&q=bar
+# you would receive the multiple q query parameters' values (foo and bar) in
+# a Python list inside your path operation function, in the function parameter q.
+
+#! To declare a query parameter with a type of list, like in the example above, you need
+#! to explicitly use Query, otherwise it would be interpreted as a request body.
+
+
+@app.get("/items/")
+async def read_items_title(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/")
+async def read_items_alias(q: Annotated[str | None, Query(alias="item-query")] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# Deprecated parameters
+@app.get("/items/")
+async def read_items_deprecated(
+    q: Annotated[
+        str | None,
+        Query(
+            alias="item-query",
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50,
+            pattern="^fixedquery$",
+            deprecated=True,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# Hidden
+@app.get("/items/")
+async def read_items_hidden(
+    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
